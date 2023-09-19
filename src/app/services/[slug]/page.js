d17d1f30267a0ad8208964a20160bdd102bf5ad0
Aneesh <brainchildofbalan@gmail.com>
2023-09-19 22:46:33 +0530

    fixing code

--- src/app/services/[slug]/page.js
+++ src/app/services/[slug]/page.js
@@ -1,12 +1,9 @@
-"use client"
 import ServiceDetails from '@components/components/pages/serviceDetails';
 import useFetch from '@components/hooks/useFetch';
 import { useParams } from 'next/navigation'
 import React from 'react'
 
 const services = async ({ params }) => {
-    // const params = useParams();
-    // // console.log(params.slug);
     const options = { cache: "no-store" };
     const data = await useFetch(`/service/${params.slug}`, options);
     return (
