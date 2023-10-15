c6b565db6e30d48af371f10abf4eacaaa3092bfd
Aneesh <brainchildofbalan@gmail.com>
2023-10-15 15:32:13 +0530

    added domain name and fixed the mistake

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost, api.nrilife.com'],
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost'
+            },
+            {
+                protocol: 'https',
+                hostname: 'api.nrilife.com'
+            },
+        ]
     },
 }
 
