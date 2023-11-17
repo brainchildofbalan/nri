b2043f895fe26d4c07f353a7a340a1c21da5e325
Aneesh <brainchildofbalan@gmail.com>
2023-11-17 15:22:27 +0530

    fixed issue in payment gatway

--- src/hooks/useMakePayment.js
+++ src/hooks/useMakePayment.js
@@ -7,67 +7,62 @@ import { useApi } from './useApi';
 export const useMakePayment = () => {
     const router = useRouter();
     const { api } = useApi()
+
+
+
+
+
     const makePayment = async ({ productId = null, invoice_id, clearCart, total }) => {
         // Make API call to the serverless API
-
-
-
-        const data = await useFetch(`/pay-now`, {
-            method: "POST",
-            mode: "cors", // no-cors, *cors, same-origin
-            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: "same-origin", // include, *same-origin, omit
-            headers: {
-                "Content-Type": "application/json",
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: "follow", // manual, *follow, error
-            referrerPolicy: "no-referrer",
+        const responseItem = await useFetch('/pay-now', {
+            method: 'POST',
             body: JSON.stringify({ productId: 123, total: total }),
-
-        })
-
-
-        const options = {
-            name: data.name,
-            currency: data.currency,
-            amount: data.amount,
-            order_id: data.id,
-            description: data.amountDesc,
-            // image: logoBase64,
-            handler: function (response) {
-                // Validate payment at server - using webhooks is a better idea.
-                // alert(response.razorpay_payment_id);
-                // alert(response.razorpay_order_id);
-                // alert(response.razorpay_signature);
-
-
-                api
-                    .post(`/cart-action/${invoice_id}`, { payer_details: response.razorpay_signature, order_id: response.razorpay_order_id, payment_status: 'paid' })
-                    .then(function (response) {
-
-                        router.push(`/success?order=${invoice_id}`);
-                        if (clearCart) {
-                            clearCart()
-                        }
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    });
-            },
-            prefill: {
-                name: "John Doe",
-                email: "jdoe@example.com",
-                contact: "9876543210",
-            },
-        };
-
-        const paymentObject = new window.Razorpay(options);
-        paymentObject.open();
-
-        paymentObject.on("payment.failed", function (response) {
-            alert("Payment failed. Please try again. Contact support for help");
         });
+        // const data = await responseItem.json();
+
+        console.log(responseItem, ' data main');
+
+
+
+
+        if (responseItem) {
+            const options = {
+                key: 'rzp_test_DDAJ0jv2qAS430',
+                amount: (total * 100).toString(), // amount in paise (100 paise = 1 INR)
+                currency: 'INR',
+                name: 'Your Company',
+                description: invoice_id,
+                order_id: responseItem.id,
+                handler: function (response) {
+                    console.log(response);
+                    // Handle the success response here
+
+                    api
+                        .post(`/cart-action/${invoice_id}`, { payer_details: response.razorpay_signature, order_id: response.razorpay_order_id, payment_status: 'paid' })
+                        .then(function (response) {
+
+                            router.push(`/success?order=${invoice_id}`);
+                            if (clearCart) {
+                                clearCart()
+                            }
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+                },
+            };
+
+            const rzp = new window.Razorpay(options);
+            rzp.open();
+
+
+            rzp.on("payment.failed", function (response) {
+                alert("Payment failed. Please try again. Contact support for help");
+            });
+        }
+
+
+
 
     };
 
